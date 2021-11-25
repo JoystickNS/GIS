ce08eabce1ef7b178ee7d02524b9540048808570
JoystickNS <deniska-97@list.ru>
2021-11-25 22:29:19 +0300

    Начало проекта 15

--- info.js
+++ info.js
@@ -2,6 +2,12 @@ const info = body.querySelector(".info");
 const offInfo = body.querySelector(".offInfo");
 
 info.onclick = () => {
+  let objectInfo = main.querySelector(".object-info");
+
+  if (objectInfo) {
+    return;
+  }
+
   const html = `
     <div class="object-info">
       <h1 class="object-info__caption caption">Информация</h1>
@@ -14,7 +20,7 @@ info.onclick = () => {
     </div>`;
 
   main.insertAdjacentHTML("beforeend", html);
-  const objectInfo = main.querySelector(".object-info");
+  objectInfo = main.querySelector(".object-info");
 
   setTimeout(() => (objectInfo.style.right = "0px"), 100);
 };
